318e3cacec91de675983d01a12d7415bc189b900
Sonikm <sonikm443@gmail.com>
2024-09-28 02:02:07 +0530

    custom api response and error handling

--- youtube-backend/src/index.js
+++ youtube-backend/src/index.js
@@ -1,15 +1,47 @@
 // require('dotenv').config({path: './env'});
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!! ", err);
+  });
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
 /*
 import express from "express";
